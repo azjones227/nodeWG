922ceec4a956738324e07449b20e9079f1f9c9da
Amos Jones <azjones227@gmail.com>
2020-09-15 01:26:00 -0400

    added getPeer function

--- WG/wireguard.js
+++ WG/wireguard.js
@@ -4,9 +4,9 @@ var write = require('./conf/conf_write')
 var read = require('./conf/conf_read')
 
 class Wireguard {
-	constructor(interface, confObj) {
-		this.interface = interface || throw new Error('Interface Required!')
-		this.confFile = `/etc/wireguard/${interface}.conf`
+	constructor(iface, confObj) {
+		this.interface = iface
+		this.confFile = `/etc/wireguard/${iface}.conf`
 		let readConf = read(this.confFile) || {}
 		this.conf = {...readConf, ...confObj || {}}
 	}
@@ -14,9 +14,27 @@ class Wireguard {
  	createPeer(p_data){
 		this.conf.peer.push(p_data)
 	}
+
+	getPeer(pubkey){
+		this.conf.Peer.forEach(function(item, index, array) {
+			result = {}
+			//console.log(item)
+  			if (item['PublicKey'] === pubkey){
+  				console.log('found the key')
+  				result = item;
+  			}
+		})
+		return result;
+	}
 }
 
 result = new Wireguard('test0')
-console.log(result.interface)
-console.log(result.confFile)
-console.log(result.conf)
+getTest = result.getPeer('AsS7aikCUrXpdfSvwFnMs0yUaoQ7ZCkoUVOmNdl7NS8=')
+
+
+// trying to access different variables
+//console.log(result.interface)
+//console.log(result.confFile)
+//console.log(result.conf.Interface.Address)
+//console.log(result.conf.Peer)
+console.log(getTest)
