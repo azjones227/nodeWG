fd4427bbc5baf8e0d346c98460ac755aa8884d82
Amos Jones <azjones227@gmail.com>
2020-09-21 19:54:06 -0400

    working on peer edit function

--- WG/wireguard.js
+++ WG/wireguard.js
@@ -15,26 +15,53 @@ class Wireguard {
 		this.conf.peer.push(p_data)
 	}
 
-	getPeer(pubkey){
-		result = {}
+	getPeer(pubkey, opt){
+		let index = null
+		let result = {}
 		for(let i = 0; i < this.conf.Peer.length; i++){
 			//console.log(this.conf.Peer[i])
   			if (this.conf.Peer[i]['PublicKey'] === pubkey){
   				//console.log('found the key')
   				result = this.conf.Peer[i];
+  				index = i;
   			}
 		}
-		return result;
+		if (opt === 'd'){
+			this.conf.Peer.splice(index, 1)	
+
+		} else {
+			return result;
+		}
+	}
+
+	deletePeer(pubkey){
+		this.getPeer(pubkey,'d')
+	}
+
+	editPeer(pubkey, p_data){
+		let target = this.getPeer(pubkey)
+		for(let item in p_data){
+			target[item] = p_data[item]
+		}
 	}
 }
 
+//testing
 result = new Wireguard('test0')
-getTest = result.getPeer('AsS7aikCUrXpdfSvwFnMs0yUaoQ7ZCkoUVOmNdl7NS8=')
 
-
-// trying to access different variables
+//trying to access different variables
 //console.log(result.interface)
 //console.log(result.confFile)
 //console.log(result.conf.Interface.Address)
 //console.log(result.conf.Peer)
-console.log(getTest)
+
+//testing peer functions
+//getTest = result.getPeer('AsS7aikCUrXpdfSvwFnMs0yUaoQ7ZCkoUVOmNdl7NS8=')
+//console.log(getTest)
+
+//result.deletePeer('AsS7aikCUrXpdfSvwFnMs0yUaoQ7ZCkoUVOmNdl7NS8=')
+result.editPeer('AsS7aikCUrXpdfSvwFnMs0yUaoQ7ZCkoUVOmNdl7NS8=',{ PublicKey: 'm7Ldqa+DaJj1sCt14vRwoBPCLuQ2GzFgu9CF6EU/GAg=',
+       Endpoint: 'ron.theta42.com:51871',
+       AllowedIPs: '172.24.0.4/32, 10.4.0.0/16' })
+
+console.log(result.conf.Peer)
